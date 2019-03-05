13159bca71689efa1dff1f3b4a6b3891a3aab376
Yahor Hlushak <47380076+tribaeka@users.noreply.github.com>
2019-03-05 13:15:29 +0300

    Update Utilites.java
    upd toBYN method

--- src/by/gsu/pms/Utilites.java
+++ src/by/gsu/pms/Utilites.java
@@ -2,6 +2,6 @@ package by.gsu.pms;
 
 public class Utilites {
     public static String toBYN(int copic){
-        return copic/100 + "." + copic%100 + " BYN";
+        return copic/100 + "." + copic/10%10 + copic % 10+" BYN";
     }
 }
